1289569dc74add173d57e7f18e30280a41b81053
don.zhang <don.zhang@yeahmobi.com>
2017-04-21 18:52:28 +0800

    test

--- src/main/java/stu/demo/Application.java
+++ src/main/java/stu/demo/Application.java
@@ -6,6 +6,16 @@ import stu.demo.client.ShutDownWork;
 public class Application {
 
 	public static void main(String[] args) {
+
+		// LinkedBlockingQueue quene = new LinkedBlockingQueue();
+		//
+		//
+		// try {
+		// quene.take();
+		// } catch (InterruptedException e) {
+		//
+		// }
+
 		Runtime.getRuntime().addShutdownHook(new ShutDownWork());
 		ClientManager.startup();
 	}
